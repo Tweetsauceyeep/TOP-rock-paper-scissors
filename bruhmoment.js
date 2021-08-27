dbd93d6d858ed0241548f9e6a66f929221ba41f6
Tweetsauce <pobusauce12@gmail.com>
2021-08-27 16:37:32 +0400

    added button functionality and score system using DOM

--- bruhmoment.js
+++ bruhmoment.js
@@ -24,34 +24,53 @@ let computerScore = 0;
 // THIs IS THE ACTUAL PLAY ROUND FUNCTION PLEASE REMEMBER THIS BRO
 function playRound(getComputerChoice, getPlayerChoice) { 
     
-    if (getComputerChoice === 'rock' && getPlayerChoice.toLowerCase() === 'scissors') { // rock to scissors comp wins
+    if (getComputerChoice === 'rock' && getPlayerChoice === 'scissors') { // rock to scissors comp wins
         computerScore++;
        return `You lose. rock beats scissors | Score: ${playerScore} to ${computerScore} | PlayerInput: ${getPlayerChoice} CompInput: ${getComputerChoice}`;
-    } else if (getComputerChoice === 'scissors' && getPlayerChoice.toLowerCase() === 'paper') { // scissors to paper comp wins
+    } else if (getComputerChoice === 'scissors' && getPlayerChoice === 'paper') { // scissors to paper comp wins
         computerScore++;
         return `You lose. scissors beats paper | Score: ${playerScore} to ${computerScore} | PlayerInput: ${getPlayerChoice} CompInput: ${getComputerChoice}`;
-    } else if (getComputerChoice === 'paper' && getPlayerChoice.toLowerCase() === 'rock') { // paper to rock comp wins
+    } else if (getComputerChoice === 'paper' && getPlayerChoice === 'rock') { // paper to rock comp wins
         computerScore++;
-        return `You lose. scissors beats paper | Score: ${playerScore} to ${computerScore} | PlayerInput: ${getPlayerChoice} CompInput: ${getComputerChoice}`;
-    } else if (getPlayerChoice.toLowerCase() === 'rock' && getComputerChoice === 'scissors') { // Player options and funcitons adasd
+        return `You lose. paper beats rock | Score: ${playerScore} to ${computerScore} | PlayerInput: ${getPlayerChoice} CompInput: ${getComputerChoice}`;
+    } else if (getPlayerChoice === 'rock' && getComputerChoice === 'scissors') { // Player options and functions 
         playerScore++;
         return `You Win. rock beats scissors | Score: ${playerScore} to ${computerScore}| PlayerInput: ${getPlayerChoice} CompInput: ${getComputerChoice}` ;
-    } else if (getPlayerChoice.toLowerCase() === 'scissors' && getComputerChoice === 'paper') {
+    } else if (getPlayerChoice === 'scissors' && getComputerChoice === 'paper') {
         playerScore++;
         return `You Win. scissors beats paper | Score: ${playerScore} to ${computerScore} | PlayerInput: ${getPlayerChoice} CompInput: ${getComputerChoice}`;
-    } else if (getPlayerChoice.toLowerCase() === 'paper' && getComputerChoice === 'rock') {
+    } else if (getPlayerChoice === 'paper' && getComputerChoice === 'rock') {
         playerScore++;
         return `You Win. paper beats rock | Score: ${playerScore} to ${computerScore} | PlayerInput: ${getPlayerChoice} CompInput: ${getComputerChoice}`;
-    } else if (getComputerChoice === getPlayerChoice.toLowerCase()) {
+    } else if (getComputerChoice === getPlayerChoice) {
         return ` Its a Draw | Score: ${playerScore} to ${computerScore} | PlayerInput: ${getPlayerChoice} CompInput: ${getComputerChoice}`;
     }
 }
+let getPlayerChoice = document.querySelectorAll('button');
+getPlayerChoice.forEach((button) =>{
+    button.addEventListener('click', ()=> {
+        getPlayerChoice = button.value;
+        const getComputerChoice =computerChoice();
+        const result = document.querySelector('#resultbox');
+        result.innerHTML = playRound(getComputerChoice, getPlayerChoice);
+        
+        /* putting the results into a div with an id of result box
+        let resultContent = playRound(getComputerChoice, getPlayerChoice);
+        const resultbox = document.querySelector('#resultbox');
+        result.append(resultContent);
+        resultbox.append(result);*/
+        })
+    });
+ 
 
 
-    function game() {
+
+
+
+
+   /* function game() { // logic that plays five rounds of the game and stuff
         for (let i = 0; i < 5; i++) {
             //function that gets player input
-            let getPlayerChoice = prompt("Rock, Paper, or Scissors?")
             //sgetPlayerChoice = getPlayerChoice.toLowerCase;
             const getComputerChoice = computerChoice(); // computer input baybee
              console.log(playRound(getComputerChoice, getPlayerChoice))
@@ -67,5 +86,5 @@ function playRound(getComputerChoice, getPlayerChoice) {
             console.log( `Its a tie | Final Score: ${playerScore} to ${computerScore}` )
         }
     }
-game()
+game()*/
 
